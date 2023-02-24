9b6cb74e36cb45d401282f7c42f116a6a2b1e7bf
Pavel Kleymenov <kleymenov.math1997@gmail.com>
2023-02-24 21:26:40 +0400

    Fixed plates.py

--- Week2/plates.py
+++ Week2/plates.py
@@ -1,6 +1,5 @@
 """Implement a program that prompts the user for a vanity plate
     and then outputs Valid if all of the requirements are met"""
-import re
 def main():
 
     # Prompt users with desired plate name
@@ -10,26 +9,50 @@ def main():
     if is_valid(plate):
         print("Valid")
 
-    # Reject othrwise
+    # Reject otherwise
     else:
         print("Invalid")
 
 def is_valid(plate):
 
+    # Slice the input from the 0th to the 2nd character
     start = plate[0:2]
-    middle = plate[len(plate)//2]
-    end = plate[-1]
+
     """Make sure plate' length stays within the range
                 of two to six characters"""
     length = len(plate)
-
-    # Make sure the input is within the range of 2 to 6 characters
-    if length >= 2 and length <= 6:
-        if start.isalpha():
-            if plate.isalnum():
-                if middle.isdigit() and end.isdigit():
-                    if plate.startswith("0", 3) and end.isalpha():
-                        return True
-    else:
+    if length < 2 or length > 6:
         return False
+
+
+    # Make sure all characters are alphanumeric
+    if not plate.isalnum():
+        return False
+
+    """Make sure all plates start with at least
+                two letters"""
+    if start.isdigit():
+        return False
+
+    # Make sure the first number in a string is not a zero
+    i = 0
+    while i < length:
+        if not plate[i].isalpha():
+            if plate[i] == "0":
+                return False
+            elif not plate[i:].isdigit():
+                return False
+            return True
+        i += 1
+
+    # Make sure numbers do not come in the middle of the plate
+    for i in range(length):
+        if plate[i].isdigit():
+            if not plate[i:].isdigit():
+                return False
+
+    # If all conditions are met, validate the plate
+    return True
+
+# Invoke the main fucntion so as to get the program running
 main()
